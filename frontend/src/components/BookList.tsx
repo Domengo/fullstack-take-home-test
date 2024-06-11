04b214f8a9d561a76aed761582af31b88b4e6f5d
Domengo <dominicsengo@gmail.com>
2024-06-11 11:14:19 +0300

    chore: Update frontend dependencies and add MUI Joy component

--- frontend/src/components/BookList.tsx
+++ frontend/src/components/BookList.tsx
@@ -10,17 +10,37 @@ import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import { createTheme, ThemeProvider } from "@mui/material";
 import Typography from "@mui/material/Typography";
+import Card from "./ui/Card";
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#3f51b5",
-    },
-    secondary: {
-      main: "#f50057",
-    },
-  },
-});
+// const theme = createTheme({
+//   palette: {
+//     primary: {
+//       main: "#3f51b5",
+//     },
+//     secondary: {
+//       main: "#f50057",
+//     },
+//   },
+//   typography: {
+//     fontFamily: "Roboto",
+//     fontSize: 16,
+//   },
+// });
+
+// theme.typography.body2 = {
+//   fontSize: "1rem",
+//   "@media (min-width:600px)": {
+//     fontSize: "1rem",
+//   },
+//   [theme.breakpoints.up("md")]: {
+//     fontSize: "1rem",
+//   },
+//   fontWeight: 500,
+//   lineHeight: 1,
+//   letterSpacing: "0.00138em",
+//   fontStyle: "italic",
+//   fontVariant: "small-caps",
+// };
 
 const BOOKS_QUERY = gql`
   query Books {
@@ -61,22 +81,29 @@ const BookList = ({
 
   if (loading) return "Loading...";
   if (error) return `Error! ${error.message}`;
-
+  // () => addBook(book)
   // console.log(books.map(book => book.coverPhotoURL));
 
   return (
-    <ThemeProvider theme={theme}>
-      <Grid container spacing={2}>
+    // <ThemeProvider theme={theme}>
+      <Grid container spacing={3}>
         {books.map((book, index) => (
-          <Grid key={index} item xs={12} sm={6} md={4} lg={3}>
-            <Box
+          <Grid key={index} item xs={12} sm={8} md={6} lg={3}>
+            <Card
+              coverPhotoURL={book.coverPhotoURL}
+              title={book.title}
+              author={book.author}
+              addBook={() => addBook(book)}
+            />
+            {/* <Box
               sx={{
                 position: "relative",
                 width: "100%",
-                height: 400,
+                height: 350,
                 overflow: "hidden",
                 borderRadius: 1,
                 color: "white",
+                boxShadow: 3,
               }}
             >
               <Box
@@ -99,12 +126,18 @@ const BookList = ({
                   bottom: 2,
                   left: 2,
                   right: 2,
+                  gap: 2,
                   zIndex: "tooltip",
                   justifyContent: "space-between",
+                  alignItems: "center",
                 }}
               >
-                <Typography variant="h4">{book.title}</Typography>
-                <Typography variant="h4">{book.author}</Typography>
+                <Typography variant="body2" gutterBottom>
+                  {book.title}
+                </Typography>
+                <Typography variant="body2" gutterBottom>
+                  {book.author}
+                </Typography>
               </Box>
 
               <IconButton
@@ -128,15 +161,16 @@ const BookList = ({
                   width: "100%",
                   height: "100%",
                   background:
-                    "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.3) 30%, rgba(0,0,0,0) 70%, rgba(0,0,0,0.5))",
+                    // "linear-gradient(rgba(0,0,0,0.9), rgba(0,0,0,0.3)30%, rgba(0,0,0,0) 80%, rgba(0,0,0,4.3))"
+                    'linear-gradient(to top, rgba(0,0,0,0.4), rgba(0,0,0,0) 100px), linear-gradient(to top, rgba(0,0,0,0.8), rgba(0,0,0,0) 200px), linear-gradient(to bottom, rgba(0,0,0,0.4), rgba(0,0,0,0) 100px), linear-gradient(to bottom, rgba(0,0,0,0.8), rgba(0,0,0,0) 100px)',
                   zIndex: "dialog",
                 }}
               />
-            </Box>
+            </Box> */}
           </Grid>
         ))}
       </Grid>
-    </ThemeProvider>
+    // </ThemeProvider>
   );
 };
 
