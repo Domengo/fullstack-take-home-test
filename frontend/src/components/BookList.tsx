5e6e189094c39de7072abdb27d174a0b6fcbe639
Domengo <dominicsengo@gmail.com>
2024-06-11 08:55:01 +0300

    chore: Update BookList component to use MUI ThemeProvider and add book details overlay

--- frontend/src/components/BookList.tsx
+++ frontend/src/components/BookList.tsx
@@ -8,7 +8,19 @@ import AddIcon from "@mui/icons-material/Add";
 import Grid from "@mui/material/Grid";
 // import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
+import { createTheme, ThemeProvider } from "@mui/material";
+import Typography from "@mui/material/Typography";
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#3f51b5",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+  },
+});
 
 const BOOKS_QUERY = gql`
   query Books {
@@ -53,94 +65,78 @@ const BookList = ({
   // console.log(books.map(book => book.coverPhotoURL));
 
   return (
-    // <List>
-    //   {books.map((book, index) => (
-    //     <ListItem key={index}>
-    //       <img src={book.coverPhotoURL} alt={book.title} style={{ width: '50px', marginRight: '10px'}}/>
-    //       <ListItemText primary={book.title} secondary={book.author} />
-    //       <IconButton aria-label="add" onClick={() => addBook(book)}>
-    //         <AddIcon color="primary" />
-    //       </IconButton>
-    //     </ListItem>
-    //   ))}
-    // </List>
-//     <Grid container spacing={2}>
-//     {books.map((book, index) => (
-//         <Grid key={index} item xs={12} sm={6} md={4} lg={3}>
-//             <Paper>
-//                 <img src={book.coverPhotoURL} alt={book.title} style={{ width: '50px', marginRight: '10px'}}/>
-//                 <div>{book.title}</div>
-//                 <div>{book.author}</div>
-//                 <IconButton aria-label="add" onClick={() => addBook(book)}>
-//                     <AddIcon color="primary" />
-//                 </IconButton>
-//             </Paper>
-//         </Grid>
-//     ))}
-// </Grid>
-<Grid container spacing={2}>
-  {books.map((book, index) => (
-    <Grid key={index} item xs={12} sm={6} md={4} lg={3}>
-      <Box 
-        sx={{ 
-          position: 'relative', 
-          width: '100%', 
-          height: 200, 
-          overflow: 'hidden', 
-          borderRadius: 1,
-          color: 'white',
-      }}>
-        <Box
-          component="img"
-          src={book.coverPhotoURL}
-          alt={book.title}
-          sx={{
-            width: '100%',
-            height: '100%',
-            objectFit: 'cover',
-            position: 'absolute',
-            zIndex: 'auto',
-          }}
-        />
+    <ThemeProvider theme={theme}>
+      <Grid container spacing={2}>
+        {books.map((book, index) => (
+          <Grid key={index} item xs={12} sm={6} md={4} lg={3}>
+            <Box
+              sx={{
+                position: "relative",
+                width: "100%",
+                height: 400,
+                overflow: "hidden",
+                borderRadius: 1,
+                color: "white",
+              }}
+            >
+              <Box
+                component="img"
+                src={book.coverPhotoURL}
+                alt={book.title}
+                sx={{
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                  position: "absolute",
+                  zIndex: "auto",
+                }}
+              />
 
-        <Box
-          sx={{
-            display: 'flex',
-            position: 'absolute',
-            bottom: 2,
-            left: 2,
-            right: 2,
-            zIndex: 'tooltip',
-            justifyContent: 'space-between',
-          }}
-        >
-          <span>{book.title}</span>
-          <span>{book.author}</span>
-        </Box>
-          
-        <IconButton 
-          aria-label="add"
-          onClick={() => addBook(book)}
-          style={{position: 'absolute', right: '10px', top: '10px'}}
-        >
-          <AddIcon color="primary" />
-        </IconButton>
+              <Box
+                sx={{
+                  display: "flex",
+                  position: "absolute",
+                  bottom: 2,
+                  left: 2,
+                  right: 2,
+                  zIndex: "tooltip",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Typography variant="h4">{book.title}</Typography>
+                <Typography variant="h4">{book.author}</Typography>
+              </Box>
 
-        <Box
-          sx={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%',
-            background: 'linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.3) 30%, rgba(0,0,0,0) 70%, rgba(0,0,0,0.5))',
-            zIndex: 'dialog',
-          }}
-        />
-      </Box>
-    </Grid>
-  ))}
-</Grid>
+              <IconButton
+                aria-label="add"
+                onClick={() => addBook(book)}
+                style={{
+                  position: "absolute",
+                  right: "10px",
+                  top: "10px",
+                  zIndex: 2,
+                }}
+              >
+                <AddIcon color="secondary" />
+              </IconButton>
+
+              <Box
+                sx={{
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  width: "100%",
+                  height: "100%",
+                  background:
+                    "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.3) 30%, rgba(0,0,0,0) 70%, rgba(0,0,0,0.5))",
+                  zIndex: "dialog",
+                }}
+              />
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
+    </ThemeProvider>
   );
 };
 
