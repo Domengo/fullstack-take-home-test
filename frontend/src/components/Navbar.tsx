301b74c0cccf0feadf11bd894f3876a56e99611e
Domengo <dominicsengo@gmail.com>
2024-06-13 01:17:14 +0300

    refactor: Remove unnecessary code and improve code readability in Navbar component

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,71 +1,13 @@
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-// import InputBase from "@mui/material/InputBase";
 import Container from "@mui/material/Container";
-// import SearchIcon from "@mui/icons-material/Search";
 import SearchBar from "./SearchBar";
 import { Link } from "react-router-dom";
 
 const Navbar = ({ setSearch }: { setSearch: (value: string) => void }) => {
-  // const handleSearch = (e: { target: { value: string } }) => {
-  //   setSearch(e.target.value);
-  // };
 
   return (
-    // <Container
-    //   sx={{
-    //     marginBottom: 2,
-    //   }}
-    // >
-    //   <AppBar position="static">
-    //     <Toolbar>
-    //       <Typography
-    //         variant="h6"
-    //         sx={{
-    //           flexGrow: 1,
-    //           display: "none",
-    //         }}
-    //       >
-    //         <Link to="/" style={{ color: "white", textDecoration: "none" }}>
-    //           Ello
-    //         </Link>
-    //       </Typography>
-    //       <Container
-    //         sx={{
-    //           position: "relative",
-    //           borderRadius: 5,
-    //           backgroundColor: "fade(#fff, 0.15)",
-    //           "&:hover": {
-    //             backgroundColor: "fade(#fff, 0.25)",
-    //           },
-    //           marginRight: 2,
-    //           marginLeft: 0,
-    //           width: "100%",
-    //         }}
-    //       >
-    //         <Container
-    //           sx={{
-    //             padding: 2,
-    //             height: "100%",
-    //             position: "absolute",
-    //             pointerEvents: "none",
-    //             display: "flex",
-    //             alignItems: "center",
-    //             justifyContent: "center",
-    //           }}
-    //         >
-    //           <SearchIcon />
-    //         </Container>
-    //         <InputBase
-    //           placeholder="Search Books…"
-    //           inputProps={{ "aria-label": "search" }}
-    //           onChange={handleSearch}
-    //         />
-    //       </Container>
-    //     </Toolbar>
-    //   </AppBar>
-    // </Container>
     <Container sx={{ marginBottom: 2 }}>
       <AppBar position="static">
         <Toolbar>
--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,17 +1,13 @@
 import { useState, useRef } from "react";
 import TextField from "@mui/material/TextField";
-// import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
-// import in
 import gsap from "gsap";
 
 const SearchBar = ({ setSearch }: { setSearch: (search: string) => void }) => {
   const [inputVisible, setInputVisible] = useState(false);
   const inputRef = useRef<HTMLInputElement>(null);
   const containerRef = useRef<HTMLFormElement>(null);
-
-  // const [input, setInput] = useState('');
 
   const handleSearch = (event: { preventDefault: () => void }) => {
     event.preventDefault();
@@ -45,13 +41,7 @@ const SearchBar = ({ setSearch }: { setSearch: (search: string) => void }) => {
         placeholder="Search Books"
         variant="outlined"
         size="small"
-        // label="search"
         onChange={handleInputChange}
-        
-        // value={input}
-        // onChange={(event) => setInput(event.target.value)}
-        // style={{ marginRight: "10px", opacity: 0, transition: 'opacity 0.5s ease' }}
-        // sx={{ width: "100%" }}
         sx={{ opacity: inputVisible ? 1 : 0, transition: 'opacity 0.5s ease', marginRight: 1 }}
       />
       <IconButton onClick={toggleInputVisibility}>
