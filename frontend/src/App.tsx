58d70cb0500a8901633fbf800214550fe35479b4
Domengo <dominicsengo@gmail.com>
2024-06-13 01:15:07 +0300

    refactor: Update App component to use MUI ThemeProvider and apply custom theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,39 +1,47 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-import Container from '@mui/material/Container';
-import { ReactNode, useState } from 'react';
-import Navbar from './components/Navbar.tsx';
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import Container from "@mui/material/Container";
+import { ReactNode, useState } from "react";
+import Navbar from "./components/Navbar.tsx";
 
-import BookList from './components/BookList.tsx';
-import ReadingList from './components/ReadingList.tsx';
-import { ThemeProvider } from '@mui/material';
-import { theme } from './components/ui/Theme.ts';
-
+import BookList from "./components/BookList.tsx";
+import ReadingList from "./components/ReadingList.tsx";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./components/ui/Theme.ts";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/',
-  cache: new InMemoryCache()
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
 });
 
 function App() {
-  const [search, setSearch] = useState('');
-  const [readingList, setReadingList] = useState<{ author: ReactNode; title: string; }[]>([]);
-  
-  const addBook = (book: { author: ReactNode; title: string; }) => setReadingList([...readingList, book]);
-  
-  const removeBook = (removedBook: { author: ReactNode; title: string; }) => {
-    setReadingList(readingList.filter(book => book.title !== removedBook.title));
-  }
+  const [search, setSearch] = useState("");
+  const [readingList, setReadingList] = useState<
+    { author: ReactNode; title: string }[]
+  >([]);
 
+  const addBook = (book: { author: ReactNode; title: string }) =>
+    setReadingList([...readingList, book]);
+
+  const removeBook = (removedBook: { author: ReactNode; title: string }) => {
+    setReadingList(
+      readingList.filter((book) => book.title !== removedBook.title)
+    );
+  };
 
   return (
     <ThemeProvider theme={theme}>
-    <ApolloProvider client={client}>
-      <Container>
-        <Navbar setSearch={setSearch}/>
-        <BookList search={search} addBook={addBook} removeBook={removeBook} readingList={readingList}/>
-        <ReadingList readingList={readingList} removeBook={removeBook} />
-      </Container>
-    </ApolloProvider>
+      <ApolloProvider client={client}>
+        <Container>
+          <Navbar setSearch={setSearch} />
+          <BookList
+            search={search}
+            addBook={addBook}
+            removeBook={removeBook}
+            readingList={readingList}
+          />
+          <ReadingList readingList={readingList} removeBook={removeBook} />
+        </Container>
+      </ApolloProvider>
     </ThemeProvider>
   );
 }
